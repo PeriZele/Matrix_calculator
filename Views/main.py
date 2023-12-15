1c46c916b4228094f656227ee04a8e2084226872
perneszmatyi <59794751+perneszmatyi@users.noreply.github.com>
2023-12-15 14:09:48 +0000

    created the framing for the gui

--- Views/main.py
+++ Views/main.py
@@ -3,9 +3,26 @@ from tkinter import ttk
 
 
 
+WINDOW_WIDTH = 900
+WINDOW_HEIGHT = 675
+
 window = tk.Tk()
 window.geometry("900x675")
 
+
+
+header_frame  = tk.Frame(window, width= WINDOW_WIDTH, height = 75,  borderwidth=3, relief="solid" )
+main_frame  = tk.Frame(window, width= WINDOW_WIDTH, height = 500,  borderwidth=3, relief="solid" )
+footer_frame  = tk.Frame(window, width= WINDOW_WIDTH, height = 100,  borderwidth=3, relief="solid" )
+
+
+
+
+
+
+header_frame.pack()
+main_frame.pack()
+footer_frame.pack()
 
 
 window.mainloop()
